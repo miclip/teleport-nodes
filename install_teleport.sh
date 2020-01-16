cb420f98468f5abea48d4c5e89f4519b4c66e7ab
Michael Lipscombe <mlipscombe@outlook.com>
2020-01-16 14:56:12 -0700

    added gitignore

---
+++ install_teleport.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+apt-get update && apt-get install -y curl libdigest-sha-perl
+curl https://get.gravitational.com/teleport-v4.2.1-linux-amd64-bin.tar.gz.sha256
+curl -O https://get.gravitational.com/teleport-v4.2.1-linux-amd64-bin.tar.gz
+shasum -a 256 teleport-v4.2.1-linux-amd64-bin.tar.gz
+tar -xzf teleport-v4.2.1-linux-amd64-bin.tar.gz
+cd teleport
+./install
